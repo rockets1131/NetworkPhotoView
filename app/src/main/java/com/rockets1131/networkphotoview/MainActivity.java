031bfe6365e4dee078936a1068fa2bf8989b1c69
rockets1131 <rockets1131@gmail.com>
2015-08-11 15:51:29 +0800

    demo

--- app/src/main/java/com/rockets1131/networkphotoview/MainActivity.java
+++ app/src/main/java/com/rockets1131/networkphotoview/MainActivity.java
@@ -5,15 +5,24 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
 import com.rockets1131.networkphotoview.widget.NetworkPhotoView;
 
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue rQueue;
+    private LruImageCache lruImageCache;
+    private ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         NetworkPhotoView networkPhotoView = (NetworkPhotoView)findViewById(R.id.networkphotoview);
+        lruImageCache = LruImageCache.instance();
+        rQueue = Volley.newRequestQueue(this);
+        imageLoader = new ImageLoader(rQueue, lruImageCache);
+        networkPhotoView.setImageUrl("http://i6.topit.me/6/5d/45/1131907198420455d6o.jpg", imageLoader);
     }
 }
